88efbc994469e677f35ff14865c0bf79f47cb239
Xavier Reyes Ochoa <xrochoa@users.noreply.github.com>
2015-03-18 21:17:32 -0700

    Added json file and jquery functionality for bootstrap arrows

---
+++ js/git.js
@@ -0,0 +1,105 @@
+var data = {
+    "objectxxx": [{
+        "Command": "init",
+        "Description": "Create an empty Git repository or reinitialize an existing one",
+        "boolxxx": true,
+        "options": [{
+            "Command": "caca1",
+            "Description": "Create an empty Git repository or reinitialize an existing one",
+            "boolxxx": true
+        }, {
+            "Command": "caca2",
+            "Description": "Create an empty Git repository or reinitialize an existing one",
+            "boolxxx": true
+        }]
+    }, {
+        "Command": "config",
+        "Description": "Configuration options",
+        "boolxxx": true,
+        "options": [{
+            "Command": "caca3",
+            "Description": "Create an empty Git repository or reinitialize an existing one",
+            "boolxxx": true
+        }, {
+            "Command": "caca4",
+            "Description": "Create an empty Git repository or reinitialize an existing one",
+            "boolxxx": true
+        }]
+    }, {
+        "Command": "help",
+        "Description": "Get help about git commands and options",
+        "boolxxx": true
+    }, {
+        "Command": "add",
+        "Description": "Add file contents to the index",
+        "boolxxx": false
+    }, {
+        "Command": "commit",
+        "Description": "Record changes to the repository",
+        "boolxxx": false
+    }, {
+        "Command": "stash",
+        "Description": "Save edits to a temporal stash",
+        "boolxxx": false
+    }, {
+        "Command": "status",
+        "Description": "Show the working tree status",
+        "boolxxx": true
+    }, {
+        "Command": "log",
+        "Description": "Show commit logs",
+        "boolxxx": true
+    }, {
+        "Command": "diff",
+        "Description": "Show changes between commits, commit and working tree, etc",
+        "boolxxx": true
+    }, {
+        "Command": "show",
+        "Description": "Show various types of objects",
+        "boolxxx": true
+    }, {
+        "Command": "grep",
+        "Description": "Print lines matching a pattern",
+        "boolxxx": true
+    }, {
+        "Command": "branch",
+        "Description": "List, create, or delete branches",
+        "boolxxx": false
+    }, {
+        "Command": "checkout",
+        "Description": "Checkout a branch or paths to the working tree",
+        "boolxxx": false
+    }, {
+        "Command": "merge",
+        "Description": "Join two or more development histories together",
+        "boolxxx": false
+    }, {
+        "Command": "clone",
+        "Description": "Clone a repository into a new directory",
+        "boolxxx": true
+    }, {
+        "Command": "fetch",
+        "Description": "Download objects and refs from another repository",
+        "boolxxx": true
+    }, {
+        "Command": "pull",
+        "Description": "Fetch from and integrate with another repository or a local branch",
+        "boolxxx": true
+    }, {
+        "Command": "push",
+        "Description": "Update remote refs along with associated objects",
+        "boolxxx": true
+    }, {
+        "Command": "reset",
+        "Description": "Reset current HEAD to the specified state",
+        "boolxxx": false
+    }, {
+        "Command": "mv",
+        "Description": "Move or rename a file, a directory, or a symlink",
+        "boolxxx": false
+    }, {
+        "Command": "rm",
+        "Description": "Remove files from the working tree and from the index",
+        "boolxxx": false
+    }]
+};
